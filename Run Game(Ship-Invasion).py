ffb924edc8ea2cfb6f74bfc3ce0beea7106e43bf
VaibhavMore2002 <morevaibhav545@gmail.com>
2024-07-09 17:40:49 +0530

    Minor Changes

--- Run Game(Ship-Invasion).py
+++ Run Game(Ship-Invasion).py
@@ -11,51 +11,61 @@ from gamestats import GameStats
 from scoreboard import Scoreboard
 from scoreboard import Quit_State
 
+
 def run_game():
-	# Initialize pygame, settings, and screen object.
-	pygame.init()
-	ai_settings = Settings()
-	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Ship-Invasion")
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Ship-Invasion")
 
-	# Set FPS
-	FPS = 60
-	clock = pygame.time.Clock()
+    # Set FPS
+    FPS = 60
+    clock = pygame.time.Clock()
 
-	# Make a ship, a group of bullets, and a group of aliens.
-	ship = Ship(ai_settings, screen)
-	bullets = Group()
-	enemy_bullets = Group()
-	aliens = Group()
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    enemy_bullets = Group()
+    aliens = Group()
 
-	# Create an instance to store game statistics, a quit statement and create a scoreboard.
-	stats = GameStats(ai_settings)
-	sb = Scoreboard(ai_settings, screen, stats)
-	qs = Quit_State(ai_settings, screen)
+    # Create an instance to store game statistics, a quit statement and create a scoreboard.
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    qs = Quit_State(ai_settings, screen)
 
-	# Create the fleet of aliens.
-	gf.create_fleet(ai_settings, screen, ship, aliens)
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
-	# Make the Play and Game Over button.
-	play_button = Button(ai_settings, screen, "|   PRESS  Enter  Key  To  START   |   USE  Spacebar  Key  To  SHOOT   |")
-	game_over = Game_Over(ai_settings, screen, "GAME - OVER   |   Ship - Invaded")
-    #\n use left-right arrowkeys to move \n use spacebar key to shoot.
-	# Load the high score.
-	gf.load_score(stats)
-	sb.prep_high_score()
-	sb.show_score()
+    # Make the Play and Game Over button.
+    play_button = Button(
+        ai_settings, screen, "|   PRESS  Enter  Key  To  START   |   USE  Spacebar  Key  To  SHOOT   |")
+    game_over = Game_Over(ai_settings, screen,
+                          "GAME - OVER   |   Ship - Invaded")
+# \n use left-right arrowkeys to move \n use spacebar key to shoot.
+    # Load the high score.
+    gf.load_score(stats)
+    sb.prep_high_score()
+    sb.show_score()
 
-	# Start the main loop for the game.
-	while True:
-		clock.tick(FPS)
-		gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets, sb, enemy_bullets )
-		if stats.game_active:
-			ship.update()
-			gf.update_bullets(ai_settings, screen, stats, sb,
-			 ship, aliens, bullets)
-			gf.update_enemy_bullets(ai_settings, screen, stats, sb, ship, aliens, enemy_bullets)
-			gf.update_aliens(ai_settings, stats, sb, screen, ship, aliens, bullets, enemy_bullets, game_over)
-		if not stats.game_active:
-			game_over.draw_button()
-		gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, qs, enemy_bullets, game_over)
+    # Start the main loop for the game.
+    while True:
+        clock.tick(FPS)
+        gf.check_events(ai_settings, screen, stats, play_button,
+                        ship, aliens, bullets, sb, enemy_bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb,
+                              ship, aliens, bullets)
+            gf.update_enemy_bullets(
+                ai_settings, screen, stats, sb, ship, aliens, enemy_bullets)
+            gf.update_aliens(ai_settings, stats, sb, screen,
+                             ship, aliens, bullets, enemy_bullets, game_over)
+        if not stats.game_active:
+            game_over.draw_button()
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens,
+                         bullets, play_button, qs, enemy_bullets, game_over)
+
+
 run_game()
--- scoreboard.py
+++ scoreboard.py
@@ -2,8 +2,10 @@ import pygame.font
 from pygame.sprite import Group
 from ship import Ship
 
+
 class Scoreboard():
     """A class to report scoring information."""
+
     def __init__(self, ai_settings, screen, stats):
         """Initialize scorekeeping attributes."""
         self.screen = screen
@@ -18,53 +20,59 @@ class Scoreboard():
         self.prep_high_score()
         self.prep_level()
         self.prep_ships()
-        
+
     def prep_score(self):
         rounded_score = int(round(self.stats.score, -1))
         score_str = "SCORE  : " + "{:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
+        self.score_image = self.font.render(
+            score_str, True, self.text_color, self.ai_settings.bg_color)
 
-		# Display the score at the top right of the screen.
+        # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
         self.score_rect.left = self.screen_rect.left + 20
         self.score_rect.top = 20
-        
+
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "HIGH  SCORE  : " + "{:,}".format(high_score)+"       |       LIVES    ====])"
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.ai_settings.bg_color)
+        high_score_str = "HIGH  SCORE  : " + \
+            "{:,}".format(high_score)+"       |       LIVES    ====)"
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.ai_settings.bg_color)
 
-		# Center the high score at the top fo the screen.
+        # Center the high score at the top fo the screen.
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
-        
+
     def prep_level(self):
         """Turn the level into a rendered image."""
         level = "LEVEL : " + str(self.stats.level)
-        self.level_image = self.font.render(level, True, self.text_color, self.ai_settings.bg_color)
+        self.level_image = self.font.render(
+            level, True, self.text_color, self.ai_settings.bg_color)
 
-		# Position the level below the score.
+        # Position the level below the score.
         self.level_rect = self.level_image.get_rect()
         self.level_rect.left = self.score_rect.left
-        self.level_rect.top = self.score_rect.bottom + 5 
-        
-    def prep_ships(self): 
-        #Show how many ships are left.
+        self.level_rect.top = self.score_rect.bottom + 5
+
+    def prep_ships(self):
+        # Show how many ships are left.
         self.ships = Group()
         for ship_number in range(self.stats.ships_left + 1):
             ship = Ship(self.ai_settings, self.screen)
-            ship.rect.x = self.ai_settings.screen_width - ship.rect.width - ship_number * ship.rect.width - 10
+            ship.rect.x = self.ai_settings.screen_width - \
+                ship.rect.width - ship_number * ship.rect.width - 10
             ship.rect.y = 10
             self.ships.add(ship)
-            
+
     def show_score(self):
-        """Draw scores and ships to the screen.""" 
-        self.screen.blit(self.score_image, self.score_rect) 
+        """Draw scores and ships to the screen."""
+        self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
         self.ships.draw(self.screen)
+
 
 class Quit_State():
     def __init__(self, ai_settings, screen):
@@ -72,16 +80,19 @@ class Quit_State():
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
 
-		# Font settings for scoring information.
+        # Font settings for scoring information.
         self.text_color = (0, 0, 0)
         self.font = pygame.font.Font('font/invasion.otf', 40)
         self.quit_statement(ai_settings)
+
     def quit_statement(self, ai_settings):
         quit_state = "|   PRESS  ' Q '  to  QUIT   |   USE  Left - Right  Arrow  Keys  To  MOVE   |"
         self.font = pygame.font.Font('font/invasion.otf', 25)
-        self.quit_state = self.font.render(quit_state, True, self.text_color, (255,97,3))
+        self.quit_state = self.font.render(
+            quit_state, True, self.text_color, (255, 97, 3))
         self.quit_rect = self.quit_state.get_rect()
         self.quit_rect.center = self.screen_rect.center
         self.quit_rect.centery = self.screen_rect.centery + 135
+
     def show_quit(self):
         self.screen.blit(self.quit_state, self.quit_rect)
